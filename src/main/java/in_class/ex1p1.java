b2113174babc90a28173272ac5d36ff647b7a02c
Noah <noahedclarke@gmail.com>
2026-09-16 09:37:39 +0100

    feat: Add ex1p1 class for number guessing game with average calculation

---
+++ src/main/java/in_class/ex1p1.java
@@ -0,0 +1,29 @@
+package in_class;
+
+import java.util.Scanner;
+
+public class ex1p1 {
+    static void main(){
+        Scanner input = new Scanner(System.in);
+        int target = -1;
+        int guess = 0;
+        int total = 0;
+        int counter = 0;
+
+        while (guess != target){
+            System.out.println("Please enter a number: ");
+            guess = input.nextInt();
+            counter += 1;
+            total += guess;
+
+            if (guess == target){
+                System.out.println("You entered the correct number");
+            }else{
+                System.out.println("Try Again");
+            }
+        }
+        int av = total/counter;
+        System.out.println("Total of numbers entered:" + total);
+        System.out.println("Average:" + av);
+    }
+}
