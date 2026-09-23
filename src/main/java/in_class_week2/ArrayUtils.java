6aff09c81bdab664aa6eaca6e9f2c3af64cfc3ca
Noah <D00279210@student.dkit.ie>
2026-09-23 10:42:14 +0100

    feat: Add displayArray method to ArrayUtils for printing string array values with positions

--- src/main/java/in_class_week2/ArrayUtils.java
+++ src/main/java/in_class_week2/ArrayUtils.java
@@ -11,4 +11,15 @@ public class ArrayUtils {
             System.out.println((i + 1) + ". " + array[i]);
         }
     }
+    public static void displayArray(String[] array){
+        /**
+         * Display each value in an array with its position
+         * Pulls in an array and cycles through each value and displays the value with its position
+         * @param String[] array - The array of strings
+         */
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + ". " + array[i]);
+        }
+    }
 }
+
--- src/main/java/in_class_week2/demointarray.java
+++ src/main/java/in_class_week2/demointarray.java
@@ -4,10 +4,14 @@ public class demointarray {
     static void main(){
         /**
          * creates an array of numbers and calls displayArray to display them
+         * creates an array of names and calls displayArray to display them
          */
         int [] nums = {423, 265, 1, 67, 100, 89};
+        String [] names = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank"};
 
 
         ArrayUtils.displayArray(nums);
+        System.out.println("---------------------------");
+        ArrayUtils.displayArray(names);
     }
 }
