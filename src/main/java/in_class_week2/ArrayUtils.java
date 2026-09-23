b0a271bb4ecfc7694d731e1a303baba6d192ccab
Noah <D00279210@student.dkit.ie>
2026-09-23 10:37:16 +0100

    feat: Add ArrayUtils class with displayArray method to print array values with positions

---
+++ src/main/java/in_class_week2/ArrayUtils.java
@@ -0,0 +1,14 @@
+package in_class_week2;
+
+public class ArrayUtils {
+    public static void displayArray(int[] array){
+        /**
+         * Display each value in an array with its position
+         * Pulls in an array and cycles through each value and displays the value with its position
+         * @param int[] array - The array of numbers
+         */
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + ". " + array[i]);
+        }
+    }
+}
---
+++ src/main/java/in_class_week2/demointarray.java
@@ -0,0 +1,13 @@
+package in_class_week2;
+
+public class demointarray {
+    static void main(){
+        /**
+         * creates an array of numbers and calls displayArray to display them
+         */
+        int [] nums = {423, 265, 1, 67, 100, 89};
+
+
+        ArrayUtils.displayArray(nums);
+    }
+}
